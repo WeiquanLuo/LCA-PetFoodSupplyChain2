f72ab76b12473c4a844289cf12d6cc83335722b6
Weiquan Luo <36089153+WeiquanLuo@users.noreply.github.com>
2021-10-31 13:56:18 -0400

    change to DBSCAN

--- Ploty_PC_w_cluster.R
+++ Ploty_PC_w_cluster.R
@@ -4,8 +4,10 @@ library(plotly)
 PC_w_cluster<-read_csv('PC_w_cluster.csv') %>% 
   mutate(cluster= cluster %>% as.factor() )
 
-g <- ggplot(data = PC_w_cluster) +
-  geom_point(aes(x= P1, y= P2, color = cluster, text = Subsector, shape= Sector)) +
-  theme(legend.title = element_blank())
+g <- ggplot(data = PC_w_cluster %>% 
+              mutate(cbind_sector= paste0("Cluster: ", cluster, " | ", Sector,": ",Subsector))) +
+  geom_point(aes(x= P1, y= P2, color = cluster, text = cbind_sector)) 
+
+#  theme(legend.title = element_blank())
 
 ggplotly(g, tooltip = c('text', 'shape'))
